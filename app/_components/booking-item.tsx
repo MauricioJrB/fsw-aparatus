3ff8aee956a7a349e1c1e5620e903e47951b813b
MauricioJrB <mjunior1205@gmail.com>
2025-11-10 22:43:13 -0300

    feat: add booking item component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import { Badge } from './ui/badge';
+import { Card } from './ui/card';
+import { Avatar, AvatarImage } from './ui/avatar';
+
+interface BookingItemProps {
+  serviceName: string;
+  barbershopName: string;
+  barbershopImageUrl: string;
+  date: Date;
+}
+
+const BookingItem = ({
+  serviceName,
+  barbershopName,
+  barbershopImageUrl,
+  date,
+}: BookingItemProps) => {
+  return (
+    <Card className="flex h-full w-full min-w-full flex-row items-center justify-between p-0">
+      {/* Left Section */}
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <Badge className="">Confirmado</Badge>
+
+        <div className="flex items-center gap-2">
+          <Avatar className="h-6 w-6">
+            <AvatarImage src={barbershopImageUrl} alt={barbershopName} />
+          </Avatar>
+          <p className="text-muted-foreground text-sm">{barbershopName}</p>
+        </div>
+      </div>
+      {/* Right Section */}
+      <div className="justigy-center flex h-full flex-col items-center border-l p-4 py-3">
+        <p className="text-xs capitalize">{date.toLocaleDateString('pt-BR', { month: 'long' })}</p>
+        <p> {date.toLocaleDateString('pt-BR', { day: '2-digit' })}</p>
+        <p className="text-xs capitalize">
+          {date.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })}
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+export default BookingItem;
